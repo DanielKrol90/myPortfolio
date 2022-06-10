0b617ad6129e530ddd4d5e26366642ab689486e4
Daniel Król <danielkrol90@gmail.com>
2022-06-11 00:12:58 +0200

    finished front site for now, time to sleep :)

---
+++ js/main.js
@@ -0,0 +1,7 @@
+//! ===== TYPING ANIMATION ===== */
+var typed = new Typed(".typing", {
+    strings: ["Tworzeniem Stron WWW", "Rozwojem Aktualnych Stron", "Frontend Developer" ],
+    typeSpeed: 100,
+    BackSpeed:60,
+    loop: true,
+});
